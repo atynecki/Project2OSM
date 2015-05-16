9f3acd737881c7bf45d3139805b68828bead3aec
ArturT <a.tynecki@intelclinic.com>
2015-05-16 18:03:34 +0200

    application structure and main

---
+++ Project2OSM/src/application/RCC.java
@@ -0,0 +1,33 @@
+/**
+ * @name RCC (red cells counter)
+ * @brief simple program for counting erythrocytes in microscopic images of the blood
+ * @version 1.0
+ *
+ * @section DESCRIPTION
+ * TODO uzupelniæ! 
+ * It is simple application enabling ....   Graphical User Interface based on MCV (Model-View-Controller) pattern. 
+ */
+
+package application;
+
+import java.awt.EventQueue;
+import mvc.*;
+
+public class RCC {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				//TODO odkomentowaæ
+				//AppModel model = new AppModel();
+				//AppView view = new AppView();
+				//AppController controller = new AppController(model, view);
+				//view.setController(controller);
+				//view.setVisible(true);
+			}
+		});
+
+	}
+
+}
